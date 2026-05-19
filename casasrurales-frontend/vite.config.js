9eded07e655be5dda0068d2dbd1bd05195335362
Santiago-Sol <santiago.solartet@uqvirtual.edu.co>
2026-05-18 23:16:30 -0500

    feat: configuarcion para el despliegue del frontend

--- casasrurales-frontend/vite.config.js
+++ casasrurales-frontend/vite.config.js
@@ -7,15 +7,15 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://casasrurales-production.up.railway.app',
         changeOrigin: true
       },
       '/auth': {
-        target: 'http://localhost:8080',
+        target: 'https://casasrurales-production.up.railway.app',
         changeOrigin: true
       },
       '/uploads': {
-        target: 'http://localhost:8080',
+        target: 'https://casasrurales-production.up.railway.app',
         changeOrigin: true
       }
     }
